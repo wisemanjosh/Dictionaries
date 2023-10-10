e1e0e99e6e077ab6fd4651dc6ee62f23830252a7
JosueLessie <joslessie@hotmail.com>
2023-10-10 21:51:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Create a dictionary called person with keys 'name', 'age', and 'city', and set corresponding values.
+# Update the 'age' value in the person dictionary to reflect your current age.
+
+person = {'name': 'John', 'age': 30, 'city': 'London'}
+person['age'] = 31  # Update the age
+
+print(person)
